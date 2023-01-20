bbfc46df1852a6f1dc6d14033c98d6c774c79de3
CameronOwens <camowens58@gmail.com>
2023-01-20 08:33:44 -0500

    initial commit

---
+++ FactoryPatternExercise2/MongoDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FactoryPatternExercise2
+{
+    public class MongoDataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product (){ Name="Oculus", Price = 300},
+            new Product (){ Name="Piano", Price = 1200},
+            new Product (){ Name="KitchenAid", Price = 700},
+            new Product (){ Name="Skateboard", Price = 100}
+
+        };
+
+        public List<Product> LoadData()
+        {
+            Console.WriteLine("I am reading date from the Mongo Data Access.");
+
+            return Products;
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("I am saving data from a Mongo Data Access.");
+        }
+    }
+}
+
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What database woul you like to use?");
+            Console.WriteLine("Type: sql");
+            Console.WriteLine("Type: mongo");
+            Console.WriteLine("Type: list");
+
+            var userInput = Console.ReadLine();
+
+            IDataAccess db = DataAccessFactory.GetDataAccessType(userInput);
+
+            var products = db.LoadData();
+            db.SaveData();
+
+            foreach(var item in products)
+            {
+                Console.WriteLine($"Name:{item.Name} Price:{item.Price}");
+            }
         }
     }
 }
